a33bb6ab930e5b4acbc4f1086d90dc1255d29176
Loki <8209083+AkanoLoki@users.noreply.github.com>
2021-02-13 20:22:55 -0800

    Implemented Data fetch

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,24 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  // initialize local storage
+  const strg = window.localStorage;
+  const arrayKey = "fetched"; // arbitrary array key name.
+
+  // fetch from remote 
+  fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(fetchedData => {
+    // response type check
+    if(strg.getItem(arrayKey) == null){
+      // store only if not in storage already
+      strg.setItem(arrayKey, JSON.stringify(fetchedData));
+    }else{
+      // do nothing
+    }});
+// data fetch finished
+
+
+//JSON parse and item creation
+
 });
